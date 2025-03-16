5986d262dff8a9d641d2cb9a9b9e1e7ac4db825b
Stanislav <setthat5@gmail.com>
2025-03-16 19:17:58 +0300

    инициализация проекта PO

---
+++ conftest.py
@@ -0,0 +1,42 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options as OptionsFirefox
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='en',
+                     help="Choose browser: '--browser_name=chrome' or '--browser_name=firefox'. Chrome is used by "
+                          "default")
+
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language: '--language=en' or '--language=ru'")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
+
+    options = Options()
+    options.add_experimental_option(
+        'prefs', {'intl.accept_languages': user_language})
+
+    options_firefox = OptionsFirefox()
+    options_firefox.set_preference("intl.accept_languages", user_language)
+
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox(options=options_firefox)
+    else:
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome(options=options)  # если не прописываем в командной строке какой браузер хотим
+        # использовать то запускается в хроме
+
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
