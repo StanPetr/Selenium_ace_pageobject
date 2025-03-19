87ae0d649df8b003abd2ce51d5b087e90fc6feec
Stanislav Petrov <setthat5@gmail.com>
2025-03-19 17:03:35 +0300

    added tests for product page

--- pages/locators.py
+++ pages/locators.py
@@ -3,8 +3,17 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    BASKET_LINK = (By.LINK_TEXT, "View basket")
 
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators():
+    PRODUCT_NAME = (By.CSS_SELECTOR, "h1:nth-child(1)")
+    ADD_BTN = (By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket")
+    PRODUCT_IN_CONFIRMATION = (By.XPATH, "//*[@id='messages']/div[1]/div/strong")
+    PRODUCT_PRICE = (By.XPATH, "//*[@id='content_inner']/article/div[1]/div[2]/p[1]")
+    BASKET_TOTAL = (By.XPATH, "//*[@id='messages']/div[3]/div/p[1]/strong")
---
+++ pages/product_page.py
@@ -0,0 +1,29 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage, ProductPageLocators):
+
+    def __init__(self, browser, url, timeout=10):
+        super().__init__(browser, url, timeout)
+
+    def should_be_promo_code_in_url(self):  # 1.1 check promo in URL
+        assert "/?promo=newYear" in self.browser.current_url, "promo code missed"
+
+    def click_button_add_to_basket(self):  # 2 click button
+        button = self.browser.find_element(*ProductPageLocators.ADD_BTN)
+        button.click()
+
+    def should_be_product_name_in_confirmation(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
+        conf_name = self.browser.find_element(*ProductPageLocators.PRODUCT_IN_CONFIRMATION)
+        print(product_name.text)
+        print(conf_name.text)
+        assert conf_name.text == product_name.text, "name of product hasn't found"
+
+    def should_be_equal_product_price_and_basket_total(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
+        basket_total = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL)
+        print(product_price.text)
+        print(basket_total.text)
+        assert product_price.text == basket_total.text, "prices are not equal"
